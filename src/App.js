1e114a98ccef28ef7290b9d39e67136840d19c0c
Isadora Ávila <isadorgravila@gmail.com>
2022-08-09 16:03:11 -0300

    Layout page timeline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Register from "./pages/Register"
-import Login from "./pages/Login"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Timeline from "./pages/Timeline";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login/>} />
-            <Route path="/signup" element={<Register/>} />    
+            <Route path="/signup" element={<Register/>} />
+            <Route path='/timeline' element={<Timeline />} /> 
         </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Timeline.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import CreatePost from '../components/CreatePost';
+import Top from '../components/Top';
+
+export default function Timeline() {
+    return (
+        <Conteiner>
+            <Top />
+            <Content>
+                <Title>timeline</Title>
+                <Sides>
+                    <RightSide>
+                        {<CreatePost />}
+                        <div>aqui vão os posts da timeline</div>
+                    </RightSide>
+                    <LeftSide></LeftSide>
+                </Sides>
+            </Content>
+        </Conteiner>
+    );
+}
+
+const Conteiner = styled.div`
+    display: flex;
+    justify-content: center;
+    background-color: #333333;
+    min-height: 100vh;
+    padding-top: 150px;
+`;
+
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Title = styled.h1`
+    font-family: 'Oswald', sans-serif;
+    font-weight: 700;
+    font-size: 43px;
+    line-height: 64px;
+    color: #ffffff;
+    margin-bottom: 40px;
+`;
+
+const Sides = styled.div`
+    display: flex;
+`;
+
+const RightSide = styled.div`
+    width: 611px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const LeftSide = styled.div`
+    width: 301px;
+    height: 406px;
+    background-color: #171717;
+    border-radius: 16px;
+    margin-left: 25px;
+`;
