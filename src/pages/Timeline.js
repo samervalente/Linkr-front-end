a8f293ae6a166566a75e510b2530ebc2f6c390b9
Isadora Ávila <isadorgravila@gmail.com>
2022-08-11 10:17:53 -0300

    solving conflicts

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -8,6 +8,10 @@ import UserContext from "../context/UserContext";
 import axios from 'axios';
 import { getTrending } from "../services/post";
 import { Oval} from "react-loader-spinner";
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
+
 
 export default function Timeline() {
   const navigate = useNavigate();
@@ -15,7 +19,10 @@ export default function Timeline() {
   const { token, imageProfile, menuDisplay, setMenuDisplay } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(true);
   const [trending, setTrending] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
+  
+  
   useEffect(() => {
     if (!token || !imageProfile) {
       navigate('/');
@@ -30,10 +37,40 @@ export default function Timeline() {
     promise.then(response => {
       setPost(response.data)
       setIsLoading(false)
+
     })
 
-    promise.catch(response => console.log("erro"))
+    
+    promise.catch((error) => {
+      console.error("error")
+      setIsModalOpen(true)
+    })
+
   }, []);
+
+  const customStyle = { 
+    content:{
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "597px",
+      height: "262px",
+      backgroundColor: "#333333",
+      borderRadius: "50px",
+      color: "white",
+      textAlign: "center",
+      fontFamily: "Lato",
+      fontSize: "25px",
+      padding: "60px",
+      fontWeight: "700",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between"
+    }
+  }
 
   useEffect( () => {
     async function fetchData(){
@@ -44,6 +81,14 @@ export default function Timeline() {
     fetchData()
     
   }, [])
+
+  function closeModal(){
+    if(isModalOpen)
+      setIsModalOpen(false)
+  }
+
+
+
 
   function checkMenu() {
     if (menuDisplay) {
@@ -70,6 +115,13 @@ export default function Timeline() {
             {!isLoading && posts.length === 0 && (
               <p>There are no posts yet</p>
             )}
+            {isModalOpen ? 
+              <Modal isOpen={isModalOpen} style={customStyle}>
+                <h2>An error occured while trying to fetch the posts, please refresh the page</h2>
+                <button onClick={closeModal}>Ok</button>
+              </Modal> 
+              : 
+              <></>}
           </RightSide>
           <LeftSide>
               <div className="trendingTitle">
@@ -117,6 +169,7 @@ const RightSide = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;  
+
 `;
 
 const LeftSide = styled.div`
