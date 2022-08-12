c59f6e6914244b603aea468306e6a34f79a1186b
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-12 14:13:51 -0300

    userPage created and redirecting by clicking

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Timeline from "./pages/Timeline";
 import TimelineByHashtag from "./pages/TimelineByHashtag";
+import User from "./pages/User.js";
 import UserContext from "./context/UserContext";
 
 function App() {
   const [token, setToken] = useState("");
   const [imageProfile, setImageProfile] = useState("");
   const [menuDisplay, setMenuDisplay] = useState(false);
-  const contextValue = { token, setToken, imageProfile, setImageProfile, menuDisplay, setMenuDisplay };
+  const contextValue = {
+    token,
+    setToken,
+    imageProfile,
+    setImageProfile,
+    menuDisplay,
+    setMenuDisplay,
+  };
   return (
     <UserContext.Provider value={contextValue}>
       <BrowserRouter>
@@ -19,6 +27,7 @@ function App() {
           <Route path="/signup" element={<Register />} />
           <Route path="/timeline" element={<Timeline />} />
           <Route path="/hashtag/:hashtag" element={<TimelineByHashtag />} />
+          <Route path="/user/:id" element={<User />} />
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
