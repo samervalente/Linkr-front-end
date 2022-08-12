5bd9d488047f7a72c027d422c560ff873be1f1d1
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-12 15:53:21 -0300

    pagina user

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,28 +1,275 @@
 import styled from "styled-components";
-import UserContext from "../context/UserContext";
+import CreatePost from "../components/CreatePost";
 import Top from "../components/Top";
+import FetchPosts from "../components/FetchPosts";
 import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import axios from "axios";
+import { getTrending } from "../services/post";
+import { Oval } from "react-loader-spinner";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 export default function User() {
-  const { token, imageProfile, menuDisplay, setMenuDisplay } =
+  const navigate = useNavigate();
+  const [posts, setPost] = useState([]);
+  const [userId, setUserId] = useState("");
+  const { token, imageProfile, menuDisplay, setMenuDisplay, setPage } =
     useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [trending, setTrending] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [fetchDependency, setDependency] = useState(false);
+  const { id } = useParams();
+  console.log(posts);
+  useEffect(() => {
+    if (!token || !imageProfile) {
+      setPage(`/user/${id}`);
+      navigate("/");
+      return;
+    }
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const promise = axios.get(`http://localhost:4000/user/posts/${id}`, config);
+    promise.then((response) => {
+      setPost(response.data.posts);
+      setUserId(response.data.userId);
+      setIsLoading(false);
+    });
+
+    promise.catch((error) => {
+      console.error("error");
+      setIsModalOpen(true);
+    });
+  }, [fetchDependency]);
+
+  const customStyle = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "597px",
+      height: "262px",
+      backgroundColor: "#333333",
+      borderRadius: "50px",
+      color: "white",
+      textAlign: "center",
+      fontFamily: "Lato",
+      fontSize: "25px",
+      padding: "60px",
+      fontWeight: "700",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+    },
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const trendingTopics = await getTrending(config);
+      setTrending(trendingTopics);
+    }
+
+    fetchData();
+  }, [fetchDependency]);
+
+  function closeModal() {
+    if (isModalOpen) setIsModalOpen(false);
+  }
 
   function checkMenu() {
     if (menuDisplay) {
       setMenuDisplay(false);
     }
   }
+
+  const trendingTopics =
+    trending.length > 0
+      ? trending.map((hashtag, index) => {
+          return (
+            <Link key={index} to={`/hashtag/${hashtag.tag}`}>
+              <li>#{hashtag.tag}</li>
+            </Link>
+          );
+        })
+      : "Searching for hashtags...";
+
   return (
-    <Container onClick={checkMenu}>
+    <Conteiner onClick={checkMenu}>
       <Top />
-    </Container>
+      <Content>
+        <Title>
+          {posts.length > 0 ? `${posts[0].name}'s posts ` : "Carregando..."}
+        </Title>
+        <Sides>
+          <RightSide>
+            {posts.length > 0 ? (
+              posts.map((post, index) => (
+                <FetchPosts
+                  key={index}
+                  post={post}
+                  userId={userId}
+                  setTrending={setTrending}
+                  setDependency={setDependency}
+                  fetchDependency={fetchDependency}
+                />
+              ))
+            ) : isLoading ? (
+              <>
+                <Load>"Carregando posts..."</Load>
+                <Oval />
+              </>
+            ) : (
+              <Load>There are no posts yet</Load>
+            )}
+            {isModalOpen ? (
+              <Modal isOpen={isModalOpen} style={customStyle}>
+                <h2>
+                  An error occured while trying to fetch the posts, please
+                  refresh the page
+                </h2>
+                <button onClick={closeModal}>Ok</button>
+              </Modal>
+            ) : (
+              <></>
+            )}
+          </RightSide>
+
+          <LeftSide>
+            <div className="trendingTitle">
+              <h1>trending</h1>
+            </div>
+            <div className="divBar"></div>
+            <ul>{trendingTopics}</ul>
+          </LeftSide>
+        </Sides>
+      </Content>
+    </Conteiner>
   );
 }
 
-const Container = styled.div`
+const Conteiner = styled.div`
   display: flex;
   justify-content: center;
   background-color: #333333;
   min-height: 100vh;
   padding-top: 150px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-family: "Oswald", sans-serif;
+  font-weight: 700;
+  font-size: 43px;
+  line-height: 64px;
+  color: #ffffff;
+  margin-bottom: 40px;
+`;
+
+const Sides = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const RightSide = styled.div`
+  width: 611px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LeftSide = styled.div`
+  width: 301px;
+  height: 406px;
+  background-color: #171717;
+  border-radius: 16px;
+  margin-left: 25px;
+  color: white;
+
+  font-family: "Oswald";
+
+  .trendingTitle {
+    padding: 10px 16px;
+    h1 {
+      font-size: 27px;
+    }
+  }
+
+  .divBar {
+    width: 100%;
+    background: rgba(72, 72, 72, 1);
+    border: 1px solid rgba(72, 72, 72, 1);
+  }
+
+  ul {
+    height: 80%;
+    padding: 10px 16px;
+
+    li {
+      cursor: pointer;
+      margin-bottom: 12px;
+      font-size: 19px;
+    }
+
+    a {
+      text-decoration: none;
+      color: white;
+    }
+
+    overflow: auto;
+
+    ::-webkit-scrollbar {
+      background: none;
+      width: 8px;
+      height: 2 em;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: #171717;
+      border-radius: 100vw;
+      margin-block: 0.5em;
+    }
+
+    :-webkit-scrollbar-thumb {
+      width: 5px;
+      background: hsl(120 100% 20% / 1);
+      border: 0.25em solid red 3px;
+      border-radius: 100vw;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: var(--ligthgray);
+
+      border-radius: 16px;
+    }
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+`;
+
+const Load = styled.div`
+  font-family: "Lato";
+  color: #ffffff;
+  margin-top: 20px;
+  margin-bottom: 40px;
 `;
